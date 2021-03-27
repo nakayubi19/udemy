2bd7b7dd841cd68b7907fc86c44a40acb79c0fcf
nakayubi19 <64730439+nakayubi19@users.noreply.github.com>
2021-03-28 07:58:43 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>こんにちは!</h1>
+      <p>お元気ですか</p>
+    </div>
+  );
+};
+
+ReactDom.render(<App />, document.getElementById("root"));
